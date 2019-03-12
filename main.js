e6ca6800270017917ed31d6312140afdb6771f85
Andjelka90 <46249123+Andjelka90@users.noreply.github.com>
2019-03-12 17:24:57 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,48 +1,80 @@
-$(document).ready(function(){
-    $("#click").click(function(){
-      $("p").toggle();
-    });
+$(document).ready(function () {
+  $("#click").click(function () {
+    $("p").toggle();
   });
+});
 
 
 // VARIJABLE 
 
-const naslovFilma = document.getElementById('title');
+// const naslovFilma = document.getElementById('title');
 
-const forma = document.getElementById('forma');
-const nazivFilma = document.getElementById('naziv filma');
-const dugme = document.getElementById('dugme');
+// const forma = document.getElementById('forma');
+// const nazivFilma = document.getElementById('naziv filma');
+// const dugme = document.getElementById('dugme');
 
 
 //FUNKCIJE
 
-function ucitajPodatke(){
-    const naslov = nazivFilma.value;
-    
-    naslovFilma.innerText = naslov + ' - ' +;     
-      
-    const url = `https://api.lyrics.ovh/v1/${naslov}`;    
-    //console.log(url);    
-    fetch(url)
-    
-    .then(response => response.json())
-    
-    .then(objekat => {
-        naslovFilma.innerText = naslov + ' - ';
-        // tekstPesme.innerText = objekat.lyrics ? objekat.lyrics : "Ne postoji tekst za traženu pesmu. Pokušajte ponovo!"
+// function ucitajPodatke() {
+//   const naslov = nazivFilma.value;
+
+//   naslovFilma.innerText = naslov + ' - ' +;
+
+//   const url = `https://baza-filmova.herokuapp.com/filmovi/${naslov}`;
+  //console.log(url);    
+  // fetch(url)
+
+  //   .then(response => response.json())
+
+  // .then(objekat => {
+  //     naslovFilma.innerText = naslov + ' - ';
+  // tekstPesme.innerText = objekat.lyrics ? objekat.lyrics : "Ne postoji tekst za traženu pesmu. Pokušajte ponovo!"
 });
 }
 
-forma.addEventListener('submit', function(event){
-    event.preventDefault(); 
-    ucitajPodatke();
-});
+// forma.addEventListener('submit', function (event) {
+//   event.preventDefault();
+//   ucitajPodatke();
+// });
 
-dugme.addEventListener('click', ucitajPodatke);
+// dugme.addEventListener('click', ucitajPodatke);
 
 
 function myFunction() {
-    var x = document.createElement("INPUT");
-    x.setAttribute("type", "file");
-    document.body.appendChild(x);
+  var x = document.createElement("INPUT");
+  x.setAttribute("type", "file");
+  document.body.appendChild(x);
+}
+
+
+fetch('https://baza-filmova.herokuapp.com/filmovi/')
+  .then()
+
+
+
+const prikaz = document.getElementById('prikaz')
+const kriterij = document.getElementById('kriterij')
+
+let sviFilmovi = []
+let rezultati = []
+
+function render(niz) {
+  let sablon = ''
+  for (var i = 0; i < niz.length; i++) {
+    sablon += `<h3>${niz[i].naziv}</h3>`
   }
+  prikaz.innerHTML = sablon
+}
+
+fetch('https://baza-filmova.herokuapp.com/filmovi/ ')
+  .then(res => res.json())
+  .then(data => {
+    sviFilmovi = rezultati = data
+    render(rezultati)
+  })
+
+kriterij.addEventListener('input', function () {
+  rezultati = sviFilmovi.filter(film => film.naziv.includes(kriterij.value))
+  render(rezultati)
+})
